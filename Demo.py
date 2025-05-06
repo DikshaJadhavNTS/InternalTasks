c4436a023e2aa0457bb764a2a1caa32747446647
DikshaJadhavNTS <diksha.jadhav@neutrinotechlabs.com>
2025-05-07 00:45:35 +0530

    Task completed for git

---
+++ Demo.py
@@ -0,0 +1,10 @@
+x=2
+y=2
+print(id(x))
+print(id(y))
+list1=[1,2,3,4]
+list2=list1
+list.append
+print(list1)
+
+# v
---
+++ If_check_Password.py
@@ -0,0 +1,19 @@
+
+UserName={'Mrunal':'xyz', 'Diksha':'pqrs', 'Preti':'abcd'}
+
+
+u = input("Enter username: ")
+p = input("Enter password: ")
+
+
+if u in UserName and UserName[u] == p:
+    print(" Authentication successful")
+else:
+    print(" Authentication failed!")
+
+# def authenticate(username, password):
+#     for i in range(len(UserName)):
+#         if UserName[i] == username and Password[i] == password:
+#             return True
+#     return False
+# v
