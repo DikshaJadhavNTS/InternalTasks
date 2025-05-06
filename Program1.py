1c3de0d405d514f92dce478401ce3fff4e9fb4cc
DikshaJadhavNTS <diksha.jadhav@neutrinotechlabs.com>
2025-05-07 00:55:46 +0530

    add arithmatic opertion

--- Program1.py
+++ Program1.py
@@ -1,19 +1,3 @@
-# def find_words(letters, word_list):
-#     for word in word_list:
-#         found = True
-#         for char in word:
-#             if letters.count(char) < word.count(char):
-#                 found = False
-#                 break
-#         if found:
-#             print(word)
-
-# text = "aappbloegfdshfujsasiffjfak"
-# list1 = ['apple', 'orange']
-
-find_words(text, list1)
-find_words(text, list1)
-
 def find_words(letters, word_list):
     for word in word_list:
         found = True
@@ -28,5 +12,5 @@ text = "aappbloegfdshfujsasiffjfak"
 list1 = ['apple', 'orange']
 
 find_words(text, list1)
-find_words(text, list1)
+
 
