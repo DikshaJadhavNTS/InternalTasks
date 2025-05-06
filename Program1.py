6db47a50b35fad6610dc114a08d5a789abad4ace
DikshaJadhavNTS <diksha.jadhav@neutrinotechlabs.com>
2025-05-07 00:52:30 +0530

    Task completed

--- Program1.py
+++ Program1.py
@@ -1,3 +1,19 @@
+# def find_words(letters, word_list):
+#     for word in word_list:
+#         found = True
+#         for char in word:
+#             if letters.count(char) < word.count(char):
+#                 found = False
+#                 break
+#         if found:
+#             print(word)
+
+# text = "aappbloegfdshfujsasiffjfak"
+# list1 = ['apple', 'orange']
+
+find_words(text, list1)
+find_words(text, list1)
+
 def find_words(letters, word_list):
     for word in word_list:
         found = True
